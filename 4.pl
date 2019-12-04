21eed55eeb4df623b890860a542b4cd566d3b374
Chiel Bruin <git@bruin.ch>
2019-12-04 11:05:00 +0100

    Comments!

--- 4.pl
+++ 4.pl
@@ -6,27 +6,35 @@ part1(N) :-
 part2(N) :-
 	N == 2.
 
-
 password(N1, N2, A) :-
 	between(N1, N2, X),
 	itoa(X, A),
 	monotone(A),
-	count_repeat(A, F),
-	include(part2, F, B),
-	length(B, La),
-	La > 0.
 
+	% 1. Count repetitions
+	% 2. Filter repetitions on the condition
+	% 3. Check that there is at least one remaining
+	count_repeat(A, Reps),
+	include(part2, Reps, Y),
+	length(Y, L),
+	L > 0.
+
+
+% Integer to digit array
 itoa(N, A) :-
 	number_codes(N,X),
 	format('~s',[X]),
 	maplist(plus(48),A,X).
 
+% Values in the list are increasing
 monotone([]) :- true.
 monotone([_]) :- true.
 monotone([H1, H2 | T]) :-
 	H2 >= H1,
 	monotone([H2|T]).
 
+% Count repetitions of numbers in a list
+% [1,1,2,2,3,4,4,5,5,5] => [2,2,1,2,3]
 count_repeat([], []) :- true.
 count_repeat([_], [1]) :- true.
 count_repeat([H1, H2 | T], [X1 | XT]) :-
